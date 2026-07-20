c780a3f0d70416c7e889fef7260471e7fffc8ee3
Sahilkummar121 <sahilkumarmahto365@gmail.com>
2026-07-20 11:52:04 +0530

    initiated rate limiting using slowapi

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 
 from app.database import Base, engine
 from app.routers import urls, auth, admin
+from app.core.rate_limiting import limiter
+
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 
 
 @asynccontextmanager
@@ -19,9 +24,23 @@ async def lifespan(_app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.state.limiter = limiter
+
+
+@app.exception_handler(RateLimitExceeded)
+def rate_limit_handle(_request: Request, _ext: RateLimitExceeded):
+    return JSONResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content={"detail": "Too Many Requests"},
+    )
+
+
+app.add_middleware(SlowAPIMiddleware)
+
 
 @app.get("/")
-def home_page():
+@limiter.exempt
+def home_page(_request: Request):
     return {"message": "URL Shortener "}
 
 
